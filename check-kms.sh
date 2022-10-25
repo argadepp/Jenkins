4f849ec970f0288ef1695e7afc651a888b831934
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-10-25 15:52:42 +0530

    Update check-kms.sh

--- check-kms.sh
+++ check-kms.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-aws kms describe-key --key-id alias/eks-ebs-encrypt-key | grep arn | awk '{print $2}' | tr '",' ' '
+opt = aws kms describe-key --key-id alias/eks-ebs-encrypt-key | grep arn | awk '{print $2}' | tr '",' ' '
+
+echo $opt
 
