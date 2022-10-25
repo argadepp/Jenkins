b04c9ca2cb2afa306ef8522676fdf21c778cd4e5
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-10-25 17:22:10 +0530

    Update check-kms.sh

--- check-kms.sh
+++ check-kms.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-templateUrl="file://${WORKSPACE}/kms.yaml"
+id=$1
 
-echo "!!!!!!!!!!!!! ${action} of ${environment}-${stackName} stack is initiated !!!!!!!!!!!!!!!!!!!!!!!!"
-aws cloudformation "${action}"-stack \
---template-body "${templateUrl}" --region "${aws_region}" \
---stack-name "${environment}-${stackName}" \
---capabilities CAPABILITY_NAMED_IAM
+myCmd=(aws kms describe-key --key-id alias/$id)
+if "${myCmd[@]}" > myJson.file 2> error.file; then
+   echo "Present"
+    arn=$(cat $PWD/myJson.file | grep arn | awk '{print $2}' | tr '",' ' ')
+   echo "Arn=$arn"
+else
+    err="$(cat error.file)"
+    echo "It's not present , creation is initiated !!!!!!!!!"
+
+    templateUrl="file://${WORKSPACE}/kms.yaml"
+
+    echo "!!!!!!!!!!!!! ${action} of ${environment}-${stackName} stack is initiated !!!!!!!!!!!!!!!!!!!!!!!!"
+    aws cloudformation "${action}"-stack \
+    --template-body "${templateUrl}" --region "${aws_region}" \
+    --stack-name "${environment}-${stackName}" \
+    --capabilities CAPABILITY_NAMED_IAM
+fi
+
