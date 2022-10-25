308bf414739f5e8e8b01ebdc01ac6528f97836a1
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-10-25 16:34:57 +0530

    Update check-kms.sh

--- check-kms.sh
+++ check-kms.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 templateUrl="file://${WORKSPACE}/template/kms.yaml"
 
-aws '${action}'-stack --stack-name '${stackName}' --template-body "${templateUrl}" --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation '${action}'-stack --stack-name '${stackName}' --template-body "${templateUrl}" --capabilities CAPABILITY_NAMED_IAM
