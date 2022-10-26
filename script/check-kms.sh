c487751e36075bfb28a1d52b2cb50edc2ce8e56b
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-10-26 16:09:21 +0530

    Update check-kms.sh

--- script/check-kms.sh
+++ script/check-kms.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 id=$1
+roleName=$2
+instanceRole=$3
 
-myCmd=(aws kms describe-key --key-id alias/$id)
-if "${myCmd[@]}" > myJson.file 2> error.file; then
+######### Check IAM ##################
+myCmd=(aws iam get-role --role-name $roleName)
+instCheck=(aws iam get-role --role-name $instanceRole)
+
+"${myCmd[@]}" > myJson.file
+arn=$(cat $PWD/myJson.file | grep arn | awk '{print $2}' | tr '",' ' ')
+echo "Service Role Arn=$arn"
+
+"${instCheck[@]}" > myJson1.file
+ arn1=$(cat $PWD/myJson.file | grep arn | awk '{print $2}' | tr '",' ' ') 
+ echo "InstanceRole Arn=$arn1"
+######################################
+myCmd1=(aws kms describe-key --key-id alias/$id)
+if "${myCmd1[@]}" > myJson.file 2> error.file; then
    echo "Present"
     arn=$(cat $PWD/myJson.file | grep arn | awk '{print $2}' | tr '",' ' ')
    echo "Arn=$arn"
@@ -15,16 +29,13 @@ else
     echo "!!!!!!!!!!!!! ${action} of ${environment}-${stackName} stack is initiated !!!!!!!!!!!!!!!!!!!!!!!!"
     aws cloudformation "${action}"-stack \
     --template-body "${templateUrl}" --region "${aws_region}" \
-    --stack-name "eks-${environment}-${stackName}" \
+    --stack-name "eks-kms-${environment}-${stackName}"  --parameters ParameterKey=ServiceRole,ParameterValue="${arn}" ParameterKey=InstanceRole,ParameterValue="${arn1}" \
     --capabilities CAPABILITY_NAMED_IAM
     
     echo "Waiting for the '${action}' operation to complete on CloudFormation stack: ${environment}-${stackName}"
     aws cloudformation wait stack-${action}-complete \
-    --stack-name "eks-${environment}-${stackName}" \
+    --stack-name "eks-kms-${environment}-${stackName}" \
     --region ${aws_region}
     
-    echo "eks-${environment}-${stackName} is created successfully !!!!!!!!!!!!!!!!" 
+    echo "${stackName} is created successfully !!!!!!!!!!!!!!!!" 
 fi
-
-
-
