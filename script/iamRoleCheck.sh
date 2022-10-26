ec6f166f1d1aedb4d538ef2f03c9accef2a4bb29
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-10-26 15:04:04 +0530

    Update iamRoleCheck.sh

--- script/iamRoleCheck.sh
+++ script/iamRoleCheck.sh
@@ -4,14 +4,13 @@ instanceRole=$2
 echo "Checking IAM Roles"
 myCmd=(aws iam get-role --role-name $roleName)
 instCheck=(aws iam get-role --role-name $instanceRole)
-if "${myCmd[@]}" > myJson.file 2> error.file; then
+if ("${myCmd[@]}" > myJson.file 2> error.file) & ("${instCheck[@]}" > myJson1.file 2> error.file;) then
    echo "!!!Present!!!"
-    arn=$(cat $PWD/myJson.file | grep arn | awk '{print $2}' | tr '",' ' ')
+   arn=$(cat $PWD/myJson.file | grep arn | awk '{print $2}' | tr '",' ' ')
    echo "Service Role Arn=$arn"
-   if "${instCheck[@]}" > myJson.file 2> error.file; then
-       echo "!!!Present!!!"
-       arn1=$(cat $PWD/myJson.file | grep arn | awk '{print $2}' | tr '",' ' ') 
-       echo "InstanceRole Arn=$arn1"    
+   echo "!!!Present!!!"
+   arn1=$(cat $PWD/myJson1.file | grep arn | awk '{print $2}' | tr '",' ' ') 
+   echo "InstanceRole Arn=$arn1"  
 else
     err="$(cat error.file)"
     echo "It's not present , creation is initiated !!!!!!!!!"
