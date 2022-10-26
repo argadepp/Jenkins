7e3b1ed00a108bcdea1ec7a90e07718855e71224
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-10-26 15:07:45 +0530

    Update iamRoleCheck.sh

--- script/iamRoleCheck.sh
+++ script/iamRoleCheck.sh
@@ -14,7 +14,19 @@ if ("${myCmd[@]}" > myJson.file 2> error.file) & ("${instCheck[@]}" > myJson1.fi
 else
     err="$(cat error.file)"
     echo "It's not present , creation is initiated !!!!!!!!!"
-
     templateUrl="file://${WORKSPACE}/template/iamRole.yaml"
+  
+    echo "!!!!!!!!!!!!! ${action} of ${environment}-${stackName} stack is initiated !!!!!!!!!!!!!!!!!!!!!!!!"
+    aws cloudformation "${action}"-stack \
+    --template-body "${templateUrl}" --region "${aws_region}" \
+    --stack-name "${environment}-${stackName}" \
+    --capabilities CAPABILITY_NAMED_IAM
+    
+    echo "Waiting for the '${action}' operation to complete on CloudFormation stack: ${environment}-${stackName}"
+    aws cloudformation wait stack-${action}-complete \
+    --stack-name ${environment}-${stackName} \
+    --region ${aws_region}
+    
+    echo "${stackName} is created successfully !!!!!!!!!!!!!!!!"
 
 fi    
