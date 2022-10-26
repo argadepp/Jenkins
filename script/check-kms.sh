3a5e15b7b16077713a84670653a9b1949bb6a7be
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-10-26 15:41:53 +0530

    Update check-kms.sh

--- script/check-kms.sh
+++ script/check-kms.sh
@@ -15,15 +15,15 @@ else
     echo "!!!!!!!!!!!!! ${action} of ${environment}-${stackName} stack is initiated !!!!!!!!!!!!!!!!!!!!!!!!"
     aws cloudformation "${action}"-stack \
     --template-body "${templateUrl}" --region "${aws_region}" \
-    --stack-name "${environment}-${stackName}" \
+    --stack-name "eks-${environment}-${stackName}" \
     --capabilities CAPABILITY_NAMED_IAM
     
     echo "Waiting for the '${action}' operation to complete on CloudFormation stack: ${environment}-${stackName}"
     aws cloudformation wait stack-${action}-complete \
-    --stack-name ${environment}-${stackName} \
+    --stack-name "eks-${environment}-${stackName}" \
     --region ${aws_region}
     
-    echo "${stackName} is created successfully !!!!!!!!!!!!!!!!" 
+    echo "eks-${environment}-${stackName} is created successfully !!!!!!!!!!!!!!!!" 
 fi
 
 
