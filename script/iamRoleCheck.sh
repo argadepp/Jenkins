84425754cecaf2fdff2dbbcfe922772006eb3650
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-10-26 12:19:20 +0530

    Create iamRoleCheck.sh

---
+++ script/iamRoleCheck.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+roleName=$1
+instanceRole=$2
+
+myCmd=(aws iam get-role --role-name $roleName)
+instCheck=(aws iam get-role --role-name $instanceRole)
+if "${myCmd[@]}" > myJson.file 2> error.file; then
+   echo "!!!Present!!!"
+    arn=$(cat $PWD/myJson.file | grep arn | awk '{print $2}' | tr '",' ' ')
+   echo "Service Role Arn=$arn"
+   if "${instCheck[@]}" > myJson.file 2> error.file; then
+       echo "!!!Present!!!"
+       arn1=$(cat $PWD/myJson.file | grep arn | awk '{print $2}' | tr '",' ' ') 
+       echo "InstanceRole Arn=$arn1"    
+else
+    err="$(cat error.file)"
+    echo "It's not present , creation is initiated !!!!!!!!!"
+
+    templateUrl="file://${WORKSPACE}/template/iamRole.yaml"
+
+fi    
