62c1fbb168114a021cfdf95d125b4e2a068fe7d2
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-10-28 14:13:39 +0530

    Create assume_deploy_role.sh

---
+++ script/assume_deploy_role.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+role="arn:aws:iam::895321766589:role/awspratik-admin-role"
+
+echo "Assuming '${role}' with extra arguments: "
+aws sts assume-role \
+    --role-arn ${role} \
+    --role-session-name TemporarySessionKeys \
+    --output json > assume-role-output.json
