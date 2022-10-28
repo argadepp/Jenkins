2a85362d9075f667aa0f419b928e542ff812cd1a
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-10-28 17:46:15 +0530

    Update editpolicy.sh

--- script/editpolicy.sh
+++ script/editpolicy.sh
@@ -9,18 +9,18 @@ echo $check
 if [ $? -eq 0 ]
 then
   echo $PWD
-  inst=$(jq  '.Statement[1].Principal.AWS[]' $PWD/policy.json)
+  inst=$(jq  '.Statement[1].Principal.AWS[]' $PWD/script/policy.json)
   echo $inst
   instRole="arn:aws:iam::895321766589:role/$instRole"
   serviceRole="arn:aws:iam::895321766589:role/$serviceRole"
-  newinst=$(jq '.Statement[2].Principal.AWS[.Statement[2].Principal.AWS | length] |= . + "'$instRole'"'  $PWD/policy.json > $PWD/new.json)
+  newinst=$(jq '.Statement[2].Principal.AWS[.Statement[2].Principal.AWS | length] |= . + "'$instRole'"'  $PWD/script/policy.json > $PWD/script/new.json)
 
-  service=$(jq '.Statement[2].Principal.AWS[.Statement[2].Principal.AWS | length] |= . + "'$serviceRole'"'  $PWD/new.json > $PWD/final.json)
+  service=$(jq '.Statement[2].Principal.AWS[.Statement[2].Principal.AWS | length] |= . + "'$serviceRole'"'  $PWD/script/new.json > $PWD/script/final.json)
 
-  newinst=$(jq '.Statement[3].Principal.AWS[.Statement[3].Principal.AWS | length] |= . + "'$instRole'"'  $PWD/final.json > $PWD/new.json )
+  newinst=$(jq '.Statement[3].Principal.AWS[.Statement[3].Principal.AWS | length] |= . + "'$instRole'"'  $PWD/script/final.json > $PWD/script/new.json )
 
-  service=$(jq '.Statement[3].Principal.AWS[.Statement[3].Principal.AWS | length] |= . + "'$serviceRole'"'  $PWD/new.json > $PWD/policy.json)
-  changePolicy=$(aws kms get-key-policy --key-id  $id --policy-name default --output text > $PWD/policy.json)
+  service=$(jq '.Statement[3].Principal.AWS[.Statement[3].Principal.AWS | length] |= . + "'$serviceRole'"'  $PWD/script/new.json > $PWD/script/policy.json)
+  changePolicy=$(aws kms get-key-policy --key-id  $id --policy-name default --output text > $PWD/script/policy.json)
 else
     echo "already present !!!!!!!!!!!"
 fi
