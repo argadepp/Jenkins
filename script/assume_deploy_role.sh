f2cd1d188ea92a0654a2a3074860b1190c62b78c
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-10-28 14:30:15 +0530

    Update assume_deploy_role.sh

--- script/assume_deploy_role.sh
+++ script/assume_deploy_role.sh
@@ -6,4 +6,4 @@ echo "Assuming '${role}' with extra arguments: "
 aws sts assume-role \
     --role-arn ${role} \
     --role-session-name TemporarySessionKeys \
-    --output json > assume-role-output.json
+    --output json > assume-role-output1.json
