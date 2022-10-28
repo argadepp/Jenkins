8957e367d34528e1769ce876183806b57b0cf308
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-10-28 14:39:49 +0530

    Update assume_deploy_role.sh

--- script/assume_deploy_role.sh
+++ script/assume_deploy_role.sh
@@ -2,8 +2,15 @@
 
 role="arn:aws:iam::895321766589:role/awspratik-admin-role"
 
-echo "Assuming '${role}' with extra arguments: "
-aws sts assume-role \
-    --role-arn ${role} \
-    --role-session-name TemporarySessionKeys \
-    --output json > assume-role-output1.json
+# echo "Assuming '${role}' with extra arguments: "
+# aws sts assume-role \
+#     --role-arn ${role} \
+#     --role-session-name TemporarySessionKeys \
+#     --output json > assume-role-output1.json
+
+access_key=$(cat /root/.aws/credentials | grep aws_access_key_id | awk '{print $3}')
+secret_key=$(cat /root/.aws/credentials | grep aws_secret_access_key | awk '{print $3}')
+
+aws configure aws_access_key_id $access_key aws_secret_access_key $secret_key
+
+aws s3 ls
