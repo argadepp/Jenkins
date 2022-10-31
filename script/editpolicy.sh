2445f47b38e7aa628df70a56d6b0f144c15a9eb1
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-10-31 18:39:35 +0530

    Update editpolicy.sh

--- script/editpolicy.sh
+++ script/editpolicy.sh
@@ -4,20 +4,20 @@ instanceRole=$2
 id=$3
 accountNumber=$4
 echo $PWD
-ex=$(aws kms get-key-policy --key-id  $id --policy-name default --output text > policy.json)
-check=$(cat policy.json | grep "arn:aws:iam::${accountNumber}:role/$instRole" | grep "arn:aws:iam::${accountNumber}:role/$serviceRole")
+ex=$(aws kms get-key-policy --key-id  $id --policy-name default --output text > kmspolicy.json)
+check=$(cat kmspolicy.json | grep "arn:aws:iam::${accountNumber}:role/$instRole" | grep "arn:aws:iam::${accountNumber}:role/$serviceRole")
 
 echo "$?"
 echo $check
 if [ $? -eq 0 ]
 then
   echo $PWD
-  inst=$(jq  '.Statement[2].Principal.AWS[]' policy.json)
+  inst=$(jq  '.Statement[2].Principal.AWS[]' kmspolicy.json)
   echo $inst
   instRole="arn:aws:iam::${accountNumber}:role/$instanceRole"
   serviceRole="arn:aws:iam::${accountNumber}:role/$serviceRole"
   #perm=$(chmod 760 $PWD/*)
-  newinst=$(jq '.Statement[2].Principal.AWS[.Statement[2].Principal.AWS | length] |= . + "'$instRole'"'  policy.json > new.json)
+  newinst=$(jq '.Statement[2].Principal.AWS[.Statement[2].Principal.AWS | length] |= . + "'$instRole'"'  kmspolicy.json > new.json)
 
   service=$(jq '.Statement[2].Principal.AWS[.Statement[2].Principal.AWS | length] |= . + "'$serviceRole'"'  new.json > final.json)
 
