bb45d0d1d03a67f4f6989fecaab4a6ef222ad9d5
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-10-31 18:17:49 +0530

    Update editpolicy.sh

--- script/editpolicy.sh
+++ script/editpolicy.sh
@@ -4,27 +4,27 @@ instanceRole=$2
 id=$3
 accountNumber=$4
 echo $PWD
-ex=$(aws kms get-key-policy --key-id  $id --policy-name default --output text > $PWD/script/policy.json)
-check=$(cat $PWD/script/policy.json | grep "arn:aws:iam::${accountNumber}:role/$instRole" | grep "arn:aws:iam::${accountNumber}:role/$serviceRole")
+ex=$(aws kms get-key-policy --key-id  $id --policy-name default --output text > $PWD/policy.json)
+check=$(cat $PWD/policy.json | grep "arn:aws:iam::${accountNumber}:role/$instRole" | grep "arn:aws:iam::${accountNumber}:role/$serviceRole")
 
 echo "$?"
 echo $check
 if [ $? -eq 0 ]
 then
   echo $PWD
-  inst=$(jq  '.Statement[1].Principal.AWS[]' $PWD/script/policy.json)
+  inst=$(jq  '.Statement[1].Principal.AWS[]' $PWD/policy.json)
   echo $inst
   instRole="arn:aws:iam::${accountNumber}:role/$instanceRole"
   serviceRole="arn:aws:iam::${accountNumber}:role/$serviceRole"
-  #perm=$(chmod 760 $PWD/script/*)
-  newinst=$(jq '.Statement[2].Principal.AWS[.Statement[2].Principal.AWS | length] |= . + "'$instRole'"'  $PWD/script/policy.json > $PWD/script/new.json)
+  #perm=$(chmod 760 $PWD/*)
+  newinst=$(jq '.Statement[2].Principal.AWS[.Statement[2].Principal.AWS | length] |= . + "'$instRole'"'  $PWD/policy.json > $PWD/new.json)
 
-  service=$(jq '.Statement[2].Principal.AWS[.Statement[2].Principal.AWS | length] |= . + "'$serviceRole'"'  $PWD/script/new.json > $PWD/script/final.json)
+  service=$(jq '.Statement[2].Principal.AWS[.Statement[2].Principal.AWS | length] |= . + "'$serviceRole'"'  $PWD/new.json > $PWD/final.json)
 
-  newinst=$(jq '.Statement[3].Principal.AWS[.Statement[3].Principal.AWS | length] |= . + "'$instRole'"'  $PWD/script/final.json > $PWD/script/new.json )
+  newinst=$(jq '.Statement[3].Principal.AWS[.Statement[3].Principal.AWS | length] |= . + "'$instRole'"'  $PWD/final.json > $PWD/new.json )
 
-  service=$(jq '.Statement[3].Principal.AWS[.Statement[3].Principal.AWS | length] |= . + "'$serviceRole'"'  $PWD/script/new.json > $PWD/script/policy.json)
-  changePolicy=$(aws kms put-key-policy --key-id  $id --policy-name default --policy file://$PWD/script/policy.json)
+  service=$(jq '.Statement[3].Principal.AWS[.Statement[3].Principal.AWS | length] |= . + "'$serviceRole'"'  $PWD/new.json > $PWD/policy.json)
+  changePolicy=$(aws kms put-key-policy --key-id  $id --policy-name default --policy file://$PWD/policy.json)
   
   echo $changePolicy
 else
